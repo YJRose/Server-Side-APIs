bccc4b20cd3b34f04ccf5beab951ec018d0d8b33
YJRose <yj.lin963320@gmail.com>
2023-07-22 09:25:55 -0400

    get weather forcast

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ SearchBtn.addEventListener("click", function getAPI(queryURL) {
   cityName = cityName.value;
   stateCodeE1 = stateCodeE1.value;
   countryName = countryName.value;
-  var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName +"," + stateCodeE1 +","+ countryName + "&limit=1&appid=" + APIKey;
+  var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName +"," + stateCodeE1 +","+ countryName + "&appid=" + APIKey;
   
   fetch(queryURL)
   .then(function (response) {
@@ -21,7 +21,20 @@ SearchBtn.addEventListener("click", function getAPI(queryURL) {
   })
   .then(function (data) {
     console.log(data);
+    var cityNameE1 = data[0].name;
+    var cityLat = data[0].lat;
+    var cityLng = data[0].lon;
+    localStorage.setItem("Searchhistory", [ cityNameE1, cityLat, cityLng]);
     
-});
+    var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLng +"&appid=" + APIKey;
 
+    return fetch(weatherURL);
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+});
 });
